1560498cc15a144975170be84e3ac175ff28c51a
Steven Boutcher <boutchersj@gmail.com>
2022-01-25 12:01:50 -0600

    [MAIN] ready the starter project

---
+++ src/app/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>Front-End Apprenticeship</h1>
+        <h3>Pre-assessment task - User form</h3>
+        <p>Steven Boutcher</p>
+      </header>
+      <main>
+        <p>User form goes here</p>
+      </main>
+    </div>
+  );
+}
+
+export default App;
