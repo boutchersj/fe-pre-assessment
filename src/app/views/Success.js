064ab560a4cfaf684e9644e60bff0acc64c7e39d
Steven Boutcher <boutchersj@gmail.com>
2022-03-08 06:57:21 -0600

    [MAIN] Give special color styling to the Submit and Restart buttons; refine the layout for the Success view

--- src/app/views/Success.js
+++ src/app/views/Success.js
@@ -3,9 +3,9 @@ import { Link } from 'react-router-dom';
 
 function Success(props) {
   return (
-    <section>
-      <h1 data-testid='success-message'>Yay! All set!</h1>
-      <Link to='/' className='btn-nav' data-testid='restart' onClick={props.clearFormData}>Restart form</Link>
+    <section id='success-view'>
+      <h1 id='success-message' data-testid='success-message'>Yay! All set!</h1>
+      <Link to='/' id='restart' className='btn-nav' data-testid='restart' onClick={props.clearFormData}>Restart</Link>
     </section>
   );
 }
